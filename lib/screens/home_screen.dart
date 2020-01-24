1a844f4ee0cfec5cc5db994fae387e53de3523c9
myracledesign <weber.max.1988@live.de>
2020-01-24 10:04:43 +0100

    changed the appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello Youtube!"),
+        title: Text("Hello Youtube, to Flutter Explained!"),
       ),
       body: Container(),
     );
